5e5a0f1297d33a854f38e85266e1dcd166288589
maxravel <maxmaterniak@gmail.com>
2018-01-14 11:13:08 +0100

    works on total sum

--- js/js.js
+++ js/js.js
@@ -7,12 +7,14 @@
     const gaz = document.querySelector("#gaz");
     const prad = document.querySelector("#prad");
     const oplata = document.querySelector("#oplata");
+    // const calosc = document.querySelector("#calosc");
     wodaC.addEventListener("click",
     function wc(e){
         let ost = parseFloat(ostatni.value);
         let obe = parseFloat(obecny.value);
         let wynik = (obe-ost)*32.5;
-        oplata.value= wynik.toFixed(2);
+        oplata.value = wynik.toFixed(2);
+        // calosc.value=+oplata.value;
         e.preventDefault();
     }, false);
 
@@ -21,7 +23,8 @@
         let ost = parseFloat(ostatni.value);
         let obe = parseFloat(obecny.value);
         let wynik = (obe-ost)*8.91;
-        oplata.value= wynik.toFixed(2);
+        oplata.value = wynik.toFixed(2);
+        // calosc.value=+oplata.value;
         e.preventDefault();
     }, false);
 
@@ -30,7 +33,8 @@
         let ost = parseFloat(ostatni.value);
         let obe = parseFloat(obecny.value);
         let wynik = (obe-ost)*10.972*18.141/100+6.84;
-        oplata.value= wynik.toFixed(2);
+        oplata.value = wynik.toFixed(2);
+        // calosc.value=+oplata.value;
         e.preventDefault();
     }, false);
 
@@ -39,7 +43,8 @@
         let ost = parseFloat(ostatni.value);
         let obe = parseFloat(obecny.value);
         let wynik = (obe-ost)*0.58*(1+0.001*2.51)+10.7133;
-        oplata.value= wynik.toFixed(2);
+        oplata.value = wynik.toFixed(2);
+        // calosc.value=+oplata.value;
         e.preventDefault();
     }, false);
 
